75b4710badd8ffcf9c56842565dbcb445138d38f
Victor Polevoy <fx@thefx.co>
2022-10-25 20:02:33 +0200

    Add a simple example.

---
+++ examples/simple.rs
@@ -0,0 +1,12 @@
+use thread_priority;
+
+fn main() {
+    let current = thread_priority::get_current_thread_priority().unwrap();
+    println!("Current thread priority is: {current:?}");
+    let set_result = thread_priority::set_current_thread_priority(current);
+    println!(
+        "Setting this priority again: {:?}",
+        set_result, 
+    );
+    assert_eq!(set_result, Ok(()));
+}
