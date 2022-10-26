3c6303a233da1309525793631ef54cedbf1f3029
Victor Polevoy <fx@thefx.co>
2022-10-26 19:14:49 +0200

    Rename a test

--- tests/common.rs
+++ tests/common.rs
@@ -1,7 +1,7 @@
 use rstest::rstest;
 
 #[rstest]
-fn should_be_possible_reset_the_same_priority() {
+fn should_be_possible_to_reset_the_same_priority() {
     let current = thread_priority::get_current_thread_priority().unwrap();
     let set_result = thread_priority::set_current_thread_priority(current);
     assert_eq!(set_result, Ok(()));
